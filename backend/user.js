b63b6862d9e8cb72b9869b1fe42a5653c5bcf4b4
elizabethawosode <32647802+elizabethawosode@users.noreply.github.com>
2022-08-11 21:13:53 +0100

    deleted repo content

--- backend/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const user = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-});
-
-module.exports = mongoose.model("User", user);
