b0e0e03114da7bf29b2c8323a0e5f5d6044a27a6
Daniela Tripon <daniela.tripon29@gmail.com>
2022-08-13 01:26:41 +0100

    updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,6 @@ import Login from "./components/Login/Login";
 import Registration from "./components/Registration/Registration";
 import Account from "./components/Account/Account";
 import reportWebVitals from "./reportWebVitals";
-import { AuthProvider } from "./context/AuthProvider";
 
 const Routing = () => {
   return (
@@ -53,9 +52,7 @@ const Routing = () => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <Routing />
-    </AuthProvider>
+    <Routing />
   </React.StrictMode>
 );
 
